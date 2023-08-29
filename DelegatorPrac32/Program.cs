8f67bb091b3908c896e45ffeec8cd157fd4ab7a2
yejin4117 <s2108@e-mirim.hs.kr>
2023-08-29 12:11:01 +0900

    Delegator를 활용한 구현

--- DelegatorPrac32/Program.cs
+++ DelegatorPrac32/Program.cs
@@ -6,17 +6,37 @@ using System.Windows.Forms;
 
 namespace DelegatorPrac32
 {
-    internal static class Program
+    internal class Program
     {
-        /// <summary>
-        /// 해당 애플리케이션의 주 진입점입니다.
-        /// </summary>
-        [STAThread]
-        static void Main()
+        class Product
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            public string Name { get; set; }
+            public int Price { get; set; }
+        }
+        static void Main(string[] args)
+        {
+            List<Product> products = new List<Product>()
+            {
+                new Product() { Name = "감자", Price = 500 },
+                new Product() { Name = "사과", Price = 700 },
+                new Product() { Name = "고구마", Price = 400 },
+                new Product() { Name = "배추", Price = 600 },
+                new Product() { Name = "상추", Price = 300 },
+            };
+            // 정렬
+            // 1. 델리게이터를 사용한 구현
+            products.Sort(SortWithPrice);
+
+            //출력
+            foreach (var item in products)
+            {
+                Console.WriteLine(item.Name + " : " + item.Price);
+            }
+        }
+
+        private static int SortWithPrice(Product x, Product y)
+        {
+            return x.Price.CompareTo(y.Price);
         }
     }
 }
